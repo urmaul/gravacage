054352fade518170d3072af26cb4f263729fffe8
urmaul <bogdan@itechcraft.com>
2015-04-01 15:44:48 +0300

    readme and renames

--- README.md
+++ README.md
@@ -1,4 +1,36 @@
-gravacage
-=========
+# Gravacage
 
-Gravatar class that inserts Nicolas Cage into default avatars.
+Gravatar class that uses Nicolas Cage images as default avatars. Same Cage for same email forever!
+
+Images are grabbed from [gravacage.urmaul.com](http://gravacage.urmaul.com/).
+
+```php
+use urmaul\gravacage\Gravacage;
+
+// Get gravatar image url
+Gravacage::forEmail('test@test.com')->imageUrl(100);
+
+// Get Nicolas image url
+Gravacage::forEmail('test@test.com')->nicolasUrl(100);
+```
+
+### Installing via Composer
+
+The recommended way to install Gravacage is through [Composer](http://getcomposer.org).
+
+```bash
+# Install Composer
+curl -sS https://getcomposer.org/installer | php
+```
+
+Next, run the Composer command to install the latest stable version of Gravacage:
+
+```bash
+composer.phar require urmaul/gravacage
+```
+
+After installing, you need to require Composer's autoloader:
+
+```php
+require 'vendor/autoload.php';
+```
--- src/Gravacage.php
+++ src/Gravacage.php
@@ -4,37 +4,37 @@ namespace urmaul\gravacage;
 
 class Gravacage
 {
-	private $hash;
-	
-	private function __construct(){}
-	
-	/**
-	 * 
-	 * @return Gravacage
-	 */
-	public static function forEmail($email)
-	{
-		$obj = new self();
-		$obj->hash = md5($email);
-		return $obj;
-	}
-	
-	public function image($size = 80)
-	{
-		$params = array(
-			's' => $size,
-			'd' => $this->nicolasUrl($size),
-		);
-		
-		return 'http://www.gravatar.com/avatar/?' . http_build_query($params);
-	}
-	
-	protected function nicolasUrl($size = 80)
-	{
+    private $hash;
+    
+    private function __construct(){}
+    
+    /**
+     * 
+     * @return Gravacage
+     */
+    public static function forEmail($email)
+    {
+        $obj = new self();
+        $obj->hash = md5($email);
+        return $obj;
+    }
+    
+    public function imageUrl($size = 80)
+    {
+        $params = array(
+            's' => $size,
+            'd' => $this->nicolasUrl($size),
+        );
+        
+        return 'http://www.gravatar.com/avatar/?' . http_build_query($params);
+    }
+    
+    public function nicolasUrl($size = 80)
+    {
         return sprintf(
-			'http://gravacage.urmaul.com/%s/%d.jpg',
-			$size >= 100 ? 600 : 100,
-			abs(crc32($this->hash)) % 952
-		);
-	}
+            'http://gravacage.urmaul.com/%s/%d.jpg',
+            $size >= 100 ? 600 : 100,
+            abs(crc32($this->hash)) % 952
+        );
+    }
 }
